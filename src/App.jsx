dffc15d5683fd6f5c149a4429c87259ef8c797e5
stefan022 <stefan022github@gmail.com>
2023-09-03 17:09:03 +0200

    refactor App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,33 @@
-import React from 'react'
-
-// style.js
-import style from './style'
-
-// components
-import { Billing, Business, CardDeal, Clients, CTA, Footer, Hero, Navbar, Stats, Testimonials } from './components'
+import { Billing, Business, CardDeal, Clients, CTA, Footer, Hero, Navbar, Stats, Testimonials, } from "./components";
+import "./App.scss"
 
 const App = () => (
-    <div className='bg-primary w-full overflow-hidden'>
+	<div className="app">
+		<div className="app__navbar">
+			<div className="app__navbar-layout">
+				<Navbar />
+			</div>
+		</div>
 
-      <div className={`${style.paddingX} ${style.flexCenter}`}>
-        <div className={`${style.boxWidth}`}>
-          <Navbar/>
-        </div>
-      </div>
+		<div className="app__hero">
+			<div className="app__hero-layout">
+				<Hero />
+			</div>
+		</div>
 
-      <div className={`bg-primary ${style.flexStart}`}>
-        <div className={`${style.boxWidth}`}>
-          <Hero/>
-        </div>
-      </div>
+		<div className="app__section">
+			<div className="app__section-layout">
+				<Stats />
+				<Business />
+				<Billing />
+				<CardDeal />
+				<Testimonials />
+				<Clients />
+				<CTA />
+				<Footer />
+			</div>
+		</div>
+	</div>
+);
 
-      <div className={`bg-primary ${style.paddingX} ${style.flexStart}`}>
-        <div className={`${style.boxWidth}`}>
-          <Stats/>
-          <Business/>
-          <Billing/>
-          <CardDeal/>
-          <Testimonials/>
-          <Clients/>
-          <CTA/>
-          <Footer/>
-        </div>
-      </div>
-
-    </div>
-)
-
-export default App
+export default App;
